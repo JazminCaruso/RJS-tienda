2ae13901400c0423f48901486ce07f7608c00c96
Jazmin <carusojazmin@gmail.com>
2023-04-10 22:37:31 -0300

    primer componente

---
+++ src/Equipo.jsx
@@ -0,0 +1,13 @@
+const Equipo = () => {
+    return (
+        <ul>
+            <h2>Equipos</h2>
+            <hr/>
+            <li>Jaz</li>
+            <li>Luli</li>
+            <li>Fran</li>
+        </ul>
+    )
+}
+
+export default Equipo
