4f7b511b4cfe89ae35d893c633f14d0f99efceef
Jazmin <carusojazmin@gmail.com>
2023-06-04 01:25:08 -0300

    problema mayusculas

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ItemDetail from "../ItemDetail/ItemDetail"
-import Spinner from "../spinner/Spinner"
+import Spinner from "../Spinner/Spinner"
 import Error404 from "../../helpers/error"
 import { doc, getDoc } from "firebase/firestore"
 import { db } from '../../firebase/config'
